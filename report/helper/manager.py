02ea14ce3a99b11badfab149aab411fa0500cb12
dvadgama <dhaval.it@gmail.com>
2020-12-01 15:19:50 +0000

    fixed: date update bug with report Update: removed date field from export metric

--- report/helper/manager.py
+++ report/helper/manager.py
@@ -9,4 +9,10 @@ class ReportManager:
     return self.reportDates().filter(month=month)
     
   def updateReportDate(self,month,start_date,end_date):
-    ReportDate.objects.update_or_create(month=month,start_date=start_date,end_date=end_date)
+    pk_exist = ReportDate.objects.filter(pk=month)
+
+    if pk_exist:
+          pk_exist.update(start_date=start_date,end_date=end_date)
+    
+    else:
+      ReportDate.objects.update_or_create(month=month,start_date=start_date,end_date=end_date)
