58e1509fa6d00704c6a608fc702b07dbadfdab45
dvadgama <dhaval.it@gmail.com>
2020-12-04 15:05:48 +0000

    Update: optimised cost fetch process

--- aws/collector.py
+++ aws/collector.py
@@ -27,8 +27,10 @@ class Collector:
             suggested_team_names=suggested_team_names)
 
         for date in report_dates:
-            start_date = date.start_date.strftime(self.dateformat)
-            end_date = date.end_date.strftime(self.dateformat)
-            monthly_bills = self.awsCleint.aws_account_bill(
-                start_date=start_date, end_date=end_date)
-            self.awsManager.updateCost(date=date, bills=monthly_bills)
+            #Update cost for current month on each run and, for previous month only if it does not exist already
+            if date.month == 0 or not self.awsManager.getCostByMonth(month=date.month):
+                start_date = date.start_date.strftime(self.dateformat)
+                end_date = date.end_date.strftime(self.dateformat)
+                monthly_bills = self.awsCleint.aws_account_bill(
+                    start_date=start_date, end_date=end_date)
+                self.awsManager.updateCost(date=date, bills=monthly_bills)
--- gds/collector.py
+++ gds/collector.py
@@ -20,30 +20,32 @@ class Collector:
             organizations=self.gdsClient.getOrganizations())
 
         for date in report_dates:
-            start_date = date.start_date.strftime(self.dateformat)
-            end_date = date.end_date.strftime(self.dateformat)
+            #Update cost for current month on each run and, for previous month only if it does not exist already
+            if date.month == 0 or not  self.gdsManager.getCostByMonth(month=date.month):
+                start_date = date.start_date.strftime(self.dateformat)
+                end_date = date.end_date.strftime(self.dateformat)
 
-            for organization in self.gdsManager.getOgranizations():
-                organization_bill = self.gdsClient.getOrganizationBills(
-                    organization_id=organization.id, start_date=start_date, end_date=end_date)
+                for organization in self.gdsManager.getOgranizations():
+                    organization_bill = self.gdsClient.getOrganizationBills(
+                        organization_id=organization.id, start_date=start_date, end_date=end_date)
 
-                spaces = []
+                    spaces = []
 
-                for space in organization_bill:
-                    # appending space id and name
-                    spaces.append(tuple([space[0], space[1]]))
+                    for space in organization_bill:
+                        # appending space id and name
+                        spaces.append(tuple([space[0], space[1]]))
 
-                # Update Organization Spaces
-                self.gdsManager.updateOrganizationSpaces(
-                    organization=organization, spaces=spaces)
+                    # Update Organization Spaces
+                    self.gdsManager.updateOrganizationSpaces(
+                        organization=organization, spaces=spaces)
 
-                # Update Space Cost
-                for space_id, space_name, amount in organization_bill:
-                    space = self.gdsManager.getSpaceById(
-                        space_id=space_id)[0]
+                    # Update Space Cost
+                    for space_id, space_name, amount in organization_bill:
+                        space = self.gdsManager.getSpaceById(
+                            space_id=space_id)[0]
 
-                    self.gdsManager.updateCost(
-                        date=date, organization=organization, space=space, amount=amount)
+                        self.gdsManager.updateCost(
+                            date=date, organization=organization, space=space, amount=amount)
 
         suggested_team_names = self.gdsClient.suggestSpaceTeam(
             spaces=self.gdsManager.getSpaces())
